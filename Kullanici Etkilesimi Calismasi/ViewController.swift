c83d8ef95514ba38cbab2a6f2b242937c57626e2
simalceler <celersimal@gmail.com>
2023-03-06 17:52:06 +0300

    Initial Commit

---
+++ Kullanici Etkilesimi Calismasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kullanici Etkilesimi Calismasi
+//
+//  Created by Şimal Çeler on 6.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
